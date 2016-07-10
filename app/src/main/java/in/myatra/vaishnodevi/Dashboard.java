04bddc3ed50bcba98c048cf85f41d2132fc53abb
ahmedraza <raza2393@gmail.com>
2016-07-11 02:57:37 +0530

    test: adding toast message to each section of the options layout

--- app/src/main/java/in/myatra/vaishnodevi/Dashboard.java
+++ app/src/main/java/in/myatra/vaishnodevi/Dashboard.java
@@ -2,6 +2,8 @@ package in.myatra.vaishnodevi;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class Dashboard extends AppCompatActivity {
 
@@ -10,6 +12,61 @@ public class Dashboard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
     }
+
+    public void transportSec(View view){
+
+        Toast.makeText(getApplicationContext(), "Transport Section Clicked!", Toast.LENGTH_SHORT).show();
+    }
+
+
+    public void costSec(View view){
+
+        Toast.makeText(getApplicationContext(), "Cost Section Clicked!", Toast.LENGTH_SHORT).show();
+    }
+
+
+    public void accommodationSec(View view){
+
+        Toast.makeText(getApplicationContext(), "Accommodation Section Clicked!", Toast.LENGTH_SHORT).show();
+    }
+
+
+    public void mapSec(View view){
+
+        Toast.makeText(getApplicationContext(), "Map Section Clicked!", Toast.LENGTH_SHORT).show();
+    }
+
+
+    public void weatherSec(View view){
+
+        Toast.makeText(getApplicationContext(), "Weather Section Clicked!", Toast.LENGTH_SHORT).show();
+    }
+
+
+    public void checklistSec(View view){
+
+        Toast.makeText(getApplicationContext(), "Checklist Section Clicked!", Toast.LENGTH_SHORT).show();
+    }
+
+
+    public void storySec(View view){
+
+        Toast.makeText(getApplicationContext(), "Story Section Clicked!", Toast.LENGTH_SHORT).show();
+    }
+
+
+    public void nearbySec(View view){
+
+        Toast.makeText(getApplicationContext(), "Nearby Section Clicked!", Toast.LENGTH_SHORT).show();
+    }
+
+
+    public void storeSec(View view){
+
+        Toast.makeText(getApplicationContext(), "Store Section Clicked!", Toast.LENGTH_SHORT).show();
+    }
+
+
 
 
 }
